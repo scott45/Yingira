34a18130fdf23793807c87488b7a146cae2d8b59
Scott B5 <busingescott@gmail.com>
2016-10-20 11:10:27 +0300

    heroku toolbelt main

--- secret/settings.py
+++ secret/settings.py
@@ -105,3 +105,18 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
